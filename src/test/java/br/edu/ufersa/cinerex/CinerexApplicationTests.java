e657164ff2ba0e8bcb6f11457624485bf3b4a13b
AndreVidal-UFERSA <andre.vidal@alunos.ufersa.edu.br>
2026-09-06 18:34:43 -0300

    Initial commit

---
+++ src/test/java/br/edu/ufersa/cinerex/CinerexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.cinerex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinerexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
