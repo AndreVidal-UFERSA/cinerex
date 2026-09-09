31fdc2431b5ccbbda91cc15c3f639f5ba13d3e6f
AndreVidal-UFERSA <andre.vidal@alunos.ufersa.edu.br>
2026-09-09 17:23:05 -0300

    Remover excessoes

--- src/main/java/br/edu/ufersa/cinerex/domain/exception/FuncionarioJaExisteException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ufersa.cinerex.domain.exception;
-
-public class FuncionarioJaExisteException extends RuntimeException {
-    public FuncionarioJaExisteException(String message) {
-        super(message);
-    }
-}
--- src/main/java/br/edu/ufersa/cinerex/domain/exception/FuncionarioNaoExisteException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ufersa.cinerex.domain.exception;
-
-public class FuncionarioNaoExisteException extends RuntimeException {
-    public FuncionarioNaoExisteException(String message) {
-        super(message);
-    }
-}
