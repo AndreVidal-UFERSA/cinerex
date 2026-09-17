19e1db04872df024efd2911d075740fb53dcb561
AndreVidal-UFERSA <andre.vidal@alunos.ufersa.edu.br>
2026-09-16 21:53:13 -0300

    Create FuncionarioRepository.java

---
+++ src/main/java/br/edu/ufersa/cinerex/features/funcionario/FuncionarioRepository.java
@@ -0,0 +1,6 @@
+package br.edu.ufersa.cinerex.features.funcionario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+}
