199151dc7e2e394babeccf83807753c7ac879676
GustavoFernandes413 <gustavo.marreira@alunos.ufersa.edu.br>
2026-09-17 09:50:04 -0300

    Criação de FilmeRepository e SessaoRepository

---
+++ src/main/java/br/edu/ufersa/cinerex/features/filme/FilmeRepository.java
@@ -0,0 +1,6 @@
+package br.edu.ufersa.cinerex.features.filme;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilmeRepository extends JpaRepository<Filme, Long> {
+}
+
---
+++ src/main/java/br/edu/ufersa/cinerex/features/sessao/SessaoRepository.java
@@ -0,0 +1,6 @@
+package br.edu.ufersa.cinerex.features.sessao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessaoRepository extends JpaRepository<Sessao, Long> {
+}
+
