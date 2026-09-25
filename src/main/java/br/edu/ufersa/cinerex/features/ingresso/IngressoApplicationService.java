91690e7503bac241198eb660ab46767771bba57f
Acsaemilly <Acsaemilly45@gmail.com>
2026-09-25 13:44:51 -0300

    Add ingresso application service

---
+++ src/main/java/br/edu/ufersa/cinerex/features/ingresso/IngressoApplicationService.java
@@ -0,0 +1,33 @@
+package br.edu.ufersa.cinerex.features.ingresso;
+
+import br.edu.ufersa.cinerex.features.ingresso.dto.IngressoCreate;
+import br.edu.ufersa.cinerex.features.ingresso.dto.IngressoResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+class IngressoApplicationService {
+        private final IngressoRepository repository;
+        private final IngressoMapper mapper;
+
+        public IngressoApplicationService(IngressoRepository repository, IngressoMapper mapper) {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public Long criar(IngressoCreate ingressoCreate) {
+            Ingresso ingresso = mapper.toEntity(IngressoCreate);
+            Ingresso criado = repository.save(ingresso);
+            return criado.getId();
+        }
+
+        public List<IngressoResponse> listar() {
+            return repository.findAll().stream().map(mapper::toResponse).toList();
+        }
+
+        public Optional<IngressoResponse> encontrar(long id) {
+            return repository.findById(id).map(mapper::toResponse);
+        }
+}
